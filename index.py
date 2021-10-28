1b941df9034b83f76994f7042ae70837b3a405c8
zhoushuquan <zhousq@fosun.com>
2021-10-28 18:21:47 +0800

    爬个页面

---
+++ index.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import requests
+from bs4 import BeautifulSoup
+
+resp = requests.get('https://www.baidu.com')
+print(resp)
+print(resp.content)
+
+bsobj = BeautifulSoup(resp.content,'lxml')
+a_list = bsobj.find_all('a')
+text = ''
+for a in a_list:
+    href = a.get('href')
+    text += href+'\n'
+
+with open('url.text','w') as f:
+    f.write(text)
+
